5fcf05e3367b18634ac2c38a1ad8030264a6e3ce
ghoshshirshendu2 <ghosh.shirshendu2@gmail.com>
2025-07-23 11:59:30 +0530

    prometheus additions

--- FinPAL/finPalUI/finchat_app/main.py
+++ FinPAL/finPalUI/finchat_app/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from chainlit.utils import mount_chainlit
+from prometheus_fastapi_instrumentator import Instrumentator
 import uvicorn
 #from financial_planner import generate_financial_plan
 #from finPalChatNew import FinPalAgent, llm, tools, SYSTEM_MESSAGE_CONTENT,tool_map
 app = FastAPI()
-
+Instrumentator().instrument(app).expose(app)
 
 # @app.get("/app")
 # async def get_financial_plan(message: str):
